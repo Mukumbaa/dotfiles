562de5d135de6c2c051253e7697ef9850eb6979a
Mukumbaa <gabrylippolis@gmail.com>
2025-10-30 16:04:06 +0100

    up

--- migrate.sh
+++ migrate.sh
@@ -1,5 +1,5 @@
 echo "general.import = [\"~/.config/dotfiles/alacritty/alacritty.toml\"]" > ~/.config/alacritty/alacritty.toml
 cat ~/.config/dotfiles/bash/bash > ~/.bashrc
-cp ~/.config/dotfiles/starship/starship.toml ~/.config/starship.toml
+cp ~/.config/dotfiles/starship/rose-pine.toml ~/.config/starship.toml
 
 source ~/.bashrc
