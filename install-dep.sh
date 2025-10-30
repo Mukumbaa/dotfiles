bfdb865eb4cfa0f5647bc2de76e8741a781c2b6d
Mukumbaa <gabrylippolis@gmail.com>
2025-10-30 15:24:16 +0100

    up

--- install-dep.sh
+++ install-dep.sh
@@ -1,2 +1,14 @@
+#PROGRAMS
 sudo dnf copr enable solopasha/hyprland -y
 sudo dnf install hyprland hyprpaper swaylock waybar alacritty alacritty helix btop -y
+
+#LANGAUGES
+sudo dnf install gcc clangd golang -y
+
+#DOCKER
+sudo dnf -y install dnf-plugins-core
+sudo dnf config-manager --add-repo https://download.docker.com/linux/fedora/docker-ce.repo
+sudo dnf install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+sudo systemctl enable --now docker
+sudo systemctl start --now docker
+sudo usermod -aG docker $USER
