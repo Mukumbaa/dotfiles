edc2f2c0e5d1e2bc2db49c5d54c84a0642dbef05
Mukumbaa <gabrylippolis@gmail.com>
2025-10-31 14:36:20 +0100

    up

--- install.sh
+++ install.sh
@@ -8,12 +8,12 @@ elephant service enable
 systemctl --user start elephant.service
 
 ./migrate.sh
-source ~/.bashrc
-rustup-init -y
-go install golang.org/x/tools/gopls@latest && go install github.com/nametake/golangci-lint-langserver@latest && go install github.com/golangci/golangci-lint/cmd/golangci-lint@latest && go install golang.org/x/tools/cmd/goimports@latest && go install github.com/go-delve/delve/cmd/dlv@latest
-cargo install --git https://github.com/Myriad-Dreamin/tinymist --locked tinymist-cli 
-systemctl enable iwd.service
-systemctl start iwd.service
+# source ~/.bashrc
+# rustup-init -y
+# go install golang.org/x/tools/gopls@latest && go install github.com/nametake/golangci-lint-langserver@latest && go install github.com/golangci/golangci-lint/cmd/golangci-lint@latest && go install golang.org/x/tools/cmd/goimports@latest && go install github.com/go-delve/delve/cmd/dlv@latest
+# cargo install --git https://github.com/Myriad-Dreamin/tinymist --locked tinymist-cli 
+# systemctl enable iwd.service
+# systemctl start iwd.service
 #PROGRAMS
 # sudo dnf copr enable solopasha/hyprland -y
 # sudo dnf install hyprland hyprpaper swaylock waybar alacritty alacritty helix btop -y
