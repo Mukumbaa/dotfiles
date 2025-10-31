1a425c586448b06d2c025181fa9cefcb813455bf
Mukumbaa <gabrylippolis@gmail.com>
2025-10-31 18:37:52 +0100

    up

--- walker-scripts/power-menu.sh
+++ walker-scripts/power-menu.sh
@@ -1,11 +1,12 @@
-items="Shutdown\nReboot\nLock"
-output=$(echo -e $items | walker --dmenu -H)
-if [[ "$output" == "Shutdown" ]]; then
-    shutdown -h now
-elif [[ "$output" == "Reboot" ]]; then
-    reboot
-elif [[ "$output" == "Lock" ]]; then
-    swaylock
-else
-    echo "Please select a command"
-fi
+#!/bin/bash
+
+options=("Shutdown" "Reboot" "Lock")
+
+choice=$(printf "%s\n" "${options[@]}" | fzf --prompt="Choise: ")
+
+case $choice in
+  "Shutdown") shutdown -h now;;
+  "Reboot") reboot;;
+  "Lock") swaylock --image ~/.config/dotfiles/hypr/wallpaper/wall.jpg --indicator-idle-visible --indicator-radius 125 --key-hl-color=00000066 --separator-color=00000000 --inside-color=00000033 --inside-clear-color=ffffff00 --inside-caps-lock-color=ffffff00 --inside-ver-color=ffffff00 --inside-wrong-color=ffffff00 --ring-color=ffffff --ring-clear-color=ffffff --ring-caps-lock-color=ffffff --ring-ver-color=ffffff --ring-wrong-color=ffffff --line-color=00000000 --line-clear-color=ffffffFF --line-caps-lock-color=ffffffFF --line-ver-color=ffffffFF --line-wrong-color=ffffffFF --text-color=ffffff --text-clear-color=ffffff --text-ver-color=ffffff --text-wrong-color=ffffff --bs-hl-color=ffffff --caps-lock-key-hl-color=ffffffFF --caps-lock-bs-hl-color=ffffffFF --disable-caps-lock-text --text-caps-lock-color=ffffff;;
+  *) echo "Terminated";;
+esac
