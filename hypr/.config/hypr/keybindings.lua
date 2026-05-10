62c9dc0e96aaafec6a8d58c9d9c9a062c33d5bfb
Mukumbaa <gabrylippolis@gmail.com>
2026-05-10 12:59:54 +0200

    updates

--- hypr/.config/hypr/keybindings.lua
+++ hypr/.config/hypr/keybindings.lua
@@ -111,8 +111,32 @@ end)
 
 hl.on("monitor.removed", function()
     hl.dispatch(hl.dsp.focus({monitor = "eDP-1"}))
+    hl.exec_cmd("hyprctl reload")
 end)
 
+local is_mirroring = false
+local ext_monitor_name = ""
+
+hl.bind("SUPER + O", function ()
+    if not is_mirroring then
+        local monitors = hl.get_monitors()
+        for _, m in ipairs(monitors) do
+            if m.name and m.name:sub(1,3) ~= "eDP" and not m.is_mirror then
+                ext_monitor_name = m.name
+                hl.notification.create({text = m.name .. " mirroring eDP-1", duration = 2500, color = "rgb(31748f)"})
+                hl.monitor({ output = m.name, mode = "preferred", position = "auto", scale = "1", mirror = "eDP-1" })
+                is_mirroring = true
+            end
+        end
+    else
+        if ext_monitor_name ~= "" then
+            hl.notification.create({text = "Disabling mirror for " .. ext_monitor_name, duration = 2500, color = "rgb(31748f)"})
+            hl.exec_cmd("hyprctl reload")
+            is_mirroring = false
+            ext_monitor_name = ""
+        end
+    end
+end)
 -- =========================
 -- Swap window
 -- =========================
