856d8aee8eb7cb693fa1ec9c9962c36a56800682
Mukumbaa <gabrylippolis@gmail.com>
2026-05-26 21:23:48 +0200

    updates

--- nvim/.config/nvim/lua/config/keymaps.lua
+++ nvim/.config/nvim/lua/config/keymaps.lua
@@ -4,9 +4,6 @@
 vim.g.mapleader = " "
 vim.g.maplocalleader = "\\"
 
--- Up, Down
-vim.keymap.set('n', 'j', 'gj', { desc = 'Up', noremap = true})
-vim.keymap.set('n', 'k', 'gk', { desc = 'Down', noremap = true})
 
 vim.keymap.set("n", "<leader>e", ":NvimTreeToggle<CR>") --open file explorer
 vim.keymap.set("n", "<Tab>", ":bnext<CR>") -- go to next buffer
