949ab5bc5984beff3ccc4b2f04bcdc134dad66ba
Mukumbaa <gabrylippolis@gmail.com>
2026-08-21 22:29:22 +0200

    updates

--- hypr/.config/hypr/utils/lid-up.lua
+++ hypr/.config/hypr/utils/lid-up.lua
@@ -1,7 +1,15 @@
 local module = {}
 
 function module.lid_up()
-    -- hl.exec_cmd("hyprctl reload")
+    -- hl.exec_cmd("hyprctl reload") 
+    hl.monitor({
+      output = "eDP-1",
+      mode = "2880x1800@90",
+      position = "0x0",
+      scale = "1.8",
+      vrr = 0,
+      disabled = false,
+    })
     local monitors = hl.get_monitors()
 
     if #monitors == 1 then
